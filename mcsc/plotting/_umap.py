7fa080c2724e448729c0adc6db8c6b6b31b17d73
Yakir Reshef <yr073@cn002.research.partners.org>
2020-04-15 09:07:33 -0400

    umap: now takes kw params for scatter

--- mcsc/plotting/_umap.py
+++ mcsc/plotting/_umap.py
@@ -3,21 +3,43 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
-def umap(data, ix, c, alpha0=0.01, alpha1=0.1, ax=None, colorbar=True, **cbar_kw):
+def umap(data, ix, c,
+    scatter0={},
+    scatter1={},
+    ax=None, noframe=True, colorbar=True, **cbar_kw):
+    # set default plotting options
+    scatter0_ = {'alpha':0.01, 's':2, 'color':'grey'}
+    scatter1_ = {'alpha':0.1, 's':4, 'c':c[ix], 'cmap':'seismic',
+                    'vmin':-np.abs(c[ix]).max(),
+                    'vmax':np.abs(c[ix]).max()}
+    scatter0_.update(scatter0)
+    scatter1_.update(scatter1)
+
     if ax is None:
         ax = plt.gca()
-    ax.scatter(*data.obsm['X_umap'].T, color='grey', s=2, alpha=alpha0)
-    res = ax.scatter(*data.obsm['X_umap'][ix].T, c=c[ix],
-        vmin=-np.abs(c[ix]).max(), vmax=np.abs(c[ix]).max(),
-        s=4, alpha=alpha1, cmap='seismic')
+
+    # do plotting
+    ax.scatter(*data.obsm['X_umap'].T, **scatter0_)
+    res = ax.scatter(*data.obsm['X_umap'][ix].T, **scatter1_)
+
+    # remove ticks and spines
+    ax.set_xticks([]); ax.set_yticks([])
+    if noframe:
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+
+    # add colorbar if needed
     if colorbar:
         fig = plt.gcf()
         cbar = fig.colorbar(res, ax=ax, **cbar_kw)
         cbar.set_alpha(1)
         cbar.draw_all()
-        return ax, cbar
     else:
-        return ax, None
+        cbar = None
+
+    return ax, cbar
 
 def zhists(z, ts, overlays=[], propsig=None, **kwargs):
     df = pd.DataFrame(np.concatenate([z[t] for t in ts]), columns=['z'])
