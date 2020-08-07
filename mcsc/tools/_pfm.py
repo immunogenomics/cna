9ee0ca81e56446780f6321bc311910f1a2f34c5b
Yakir Reshef <yr073@cn007.research.partners.org>
2020-08-07 09:40:14 -0400

    removed linreg

--- mcsc/tools/_pfm.py
+++ mcsc/tools/_pfm.py
@@ -56,65 +56,6 @@ def pca(data, repname='sampleXnh', npcs=None):
     data.uns[repname+'_featureXpc'] = U[:,:npcs]
     data.uns[repname+'_sampleXpc'] = V[:,:npcs]
 
-def prepare(B, T, X, Y, Nnull):
-    B_oh = np.array([
-        B == b for b in np.unique(B)
-        ]).T.astype(np.float)
-    if T is None:
-        T = B_oh
-    else:
-        T = np.hstack([T, B_oh])
-
-    # residualize confounders out of X and Y
-    resid = np.eye(len(Y)) - T.dot(np.linalg.solve(T.T.dot(T), T.T))
-    Y = resid.dot(Y)
-    X = resid.dot(X)
-
-    # get null
-    NY = conditional_permutation(B, Y.astype(np.float64), Nnull).T
-
-    return X, Y, NY
-
-def linreg(data, Y, B, T, npcs=50, L=0, repname='sampleXnh', Nnull=500, newrep=None):
-    if npcs is None:
-        npcs = data.uns[repname].shape[1] - 1
-    X = data.uns[repname]
-    X, Y, NY = prepare(B, T, X, Y, Nnull)
-
-    if newrep is None:
-        newrep = repname+'.resid'
-    data.uns[newrep] = X
-    pca(data, repname=newrep, npcs=npcs)
-    X = data.uns[newrep+'_sampleXpc']
-    sqevs = data.uns[newrep+'_sqevals']
-    X *= np.sqrt(sqevs)
-
-    # compute mse
-    G = np.linalg.solve(X.T.dot(X) + L*np.eye(len(X.T)), X.T)
-    H = X.dot(G)
-    beta = G.dot(Y)
-    Yhat = H.dot(Y)
-    mse = ((Y-Yhat)**2).mean() / Y.var()
-    msemarg = ((Y[:,None] - X*beta)**2).mean(axis=0) / Y.var()
-
-    # null testing
-    nulls = []
-    nullbeta2s = []
-    for Y_ in NY:
-        beta_ = G.dot(Y_)
-        Yhat_ = H.dot(Y_)
-        mse_ = ((Y_-Yhat_)**2).mean() / Y_.var()
-        nulls.append(mse_)
-
-        msemarg_ = ((Y_[:,None] - X*beta_)**2).mean(axis=0) / Y_.var()
-        nullbeta2s.append(msemarg_)
-    nulls = np.array(nulls)
-    nullbeta2s = np.array(nullbeta2s)
-    p = ((nulls <= mse).sum() + 1) / (len(nulls)+1)
-    betap = ((nullbeta2s <= msemarg).sum(axis=0) + 1) / (len(nullbeta2s)+1)
-
-    return p, beta, betap
-
 def mixedmodel(data, Y, B, T, npcs=50, repname='sampleXnh', usepca=True,
         pval='lrt', badbatch_r2=0.05):
     if npcs is None:
