e12c194568fa83fb7d18c74d7fee259cb3d9daf2
Yakir Reshef <yr073@cn002.research.partners.org>
2020-12-20 11:20:17 -0500

    flexible aggregation for cell_to_sample

--- mcsc/preprocessing/_multisample.py
+++ mcsc/preprocessing/_multisample.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-# copies sample-level metadata that's stored per-cell from d.obs to sampleXmeta
-def cell_to_sample(data, columns, sampleXmeta='sampleXmeta', sampleid='id'):
+# converts per-cell metadata into per-sample metadata using the function specified in
+#  aggregate. columns can be either a string or an iterable of strings
+def cell_to_sample(data, columns, aggregate=np.mean,
+        sampleXmeta='sampleXmeta', sampleid='id'):
     if type(columns) == str:
         columns = [columns]
     for c in columns:
         data.uns[sampleXmeta][c] = \
-            data.obs[[sampleid, c]].drop_duplicates().set_index(sampleid)
+            data.obs[[sampleid, c]].groupby(by=sampleid).aggregate(aggregate)
 
 # compute sample size for each sample
 def sample_sizes(data, sampleid='id'):
