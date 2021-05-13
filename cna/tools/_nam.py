b59c75f207cd8a49a04aa839a8e8b6633d505a86
Miles Smith <miles-smith@omrf.org>
2021-05-13 13:17:55 -0500

    _nam.diffuse_stepwise() adjusts behavior based on AnnData version

--- cna/tools/_nam.py
+++ cna/tools/_nam.py
@@ -3,10 +3,15 @@ import warnings
 import numpy as np
 import pandas as pd
 import scipy.stats as st
+import anndata
+from packaging import version
 
 
 def diffuse_stepwise(data, s, maxnsteps=15):
-    a = data.obsp["distances"]
+    if version.parse(anndata.__version__) < version.parse("0.7.2"):
+        a = data.uns["neighbors"]["connectivities"]
+    else:
+        a = data.obsp["distances"]
     colsums = np.array(a.sum(axis=0)).flatten() + 1
 
     for i in range(maxnsteps):
