3157fdd8724a17387dbee3d3371c102cf2bc3710
Yakir Reshef <yakirr@gmail.com>
2023-09-27 12:51:22 -0400

    bugfix: numpy now doesnt like inhomogenous arrays

--- src/cna/tools/_stats.py
+++ src/cna/tools/_stats.py
@@ -5,14 +5,14 @@ def conditional_permutation(B, Y, num):
     """
     Permutes Y conditioned on B num different times.
     """
-    batchind = np.array([
+    batchind = [
         np.where(B == b)[0] for b in np.unique(B)
-        ])
+        ]
     ix = np.concatenate([
         bi[np.argsort(np.random.randn(len(bi), num), axis=0)]
         for bi in batchind
         ])
-    bix = np.zeros((len(Y), num)).astype(np.int)
+    bix = np.zeros((len(Y), num)).astype(int)
     bix[np.concatenate(batchind)] = ix
     result = Y[bix]
     return Y[bix]
